344f53e2544c243888b728adf1ee0d6ba390664c
atarico9 <atarico9@gmail.com>
2023-10-07 15:22:36 -0300

    agrega un map para mostrar los usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
 import { TwitterFollowCard } from "./TwitterFollowCard"
 
 
+const users = [
+    {
+        userName: 'atarico',
+        name: 'Agustin Tarico',
+        isFollowing: true
+    },
+    {
+        userName: 'midudev',
+        name: 'Migel Angel Duran',
+        isFollowing: false
+    },
+    {
+        userName: 'goncy',
+        name: 'Gonzalo Pozo',
+        isFollowing: false
+    },
+    {
+        userName: 's4vitar',
+        name: 'Marcelo Vazquez',
+        isFollowing: false
+    }
+]
+
+
 export const App = () => {
   return (
     <section className="followCards">
-        <TwitterFollowCard userName='atarico' initialIsFollowing={true}>
-            Agustin Tarico
-        </TwitterFollowCard>
-        
-        <TwitterFollowCard userName='midudev' >
-            Miguel Angel Duran    
-        </TwitterFollowCard>    
+        <h2>A quien seguir </h2> 
+         {
+            users.map(({ userName, name, isFollowing }) => {
+                
+                return(
+                    <TwitterFollowCard
+                    key={userName}
+                    userName={userName}
+                    initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
 
-        <TwitterFollowCard userName='goncy'>
-            Gonzalo Pozo
-        </TwitterFollowCard>    
-
-        <TwitterFollowCard userName='s4vitar'>
-            Marcelo Vazquez    
-        </TwitterFollowCard>    
+                )
+            })
+         }
     </section>
   )
 }
